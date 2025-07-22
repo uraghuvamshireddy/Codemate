5e54dd581dcd5afed0e97d19ba415ece4f1b6c67
uraghuvamshireddy <uraghuvamshireddy@gmail.com>
2025-07-22 19:13:35 +0530

    changed callbackurl to env

--- codemate/server/middleware/passport.js
+++ codemate/server/middleware/passport.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
   const email = profile.emails[0].value;
 
