6a8fd17e678fa52cb0dd0b089f1219f6fef64fdd
uraghuvamshireddy <uraghuvamshireddy@gmail.com>
2025-07-22 16:42:04 +0530

    coonected to supabase

--- codemate/server/database/db.js
+++ codemate/server/database/db.js
@@ -5,9 +5,8 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
